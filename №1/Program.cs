7d4f26ed8ab75743d743d9ec1cfe8da22c86eba5
Goose <sanya.vaganov.96@mail.ru>
2024-02-07 06:36:14 +0700

    Заключительный коммит.

---
+++ №1/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество строк:");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов:");
+int columns = int.Parse(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = random.Next(1, 100);
+        }
+    }
+Console.WriteLine("Матрица:");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("Введите позицию элемента (строка столбец):");
+    string[] position = Console.ReadLine().Split(' ');
+    int row = int.Parse(position[0]);
+    int column = int.Parse(position[1]);
+        if (row >= 0 && row < rows && column >= 0 && column < columns)
+        {
+            int element = matrix[row, column];
+            Console.WriteLine("Значение элемента: " + element);
+        }
+        else
+        {
+            Console.WriteLine("Такого элемента нет в матрице.");
+        }
---
+++ №2/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] matrix = new int[rows, columns];
+Random random = new Random();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = random.Next(10); 
+    }
+}
+
+Console.WriteLine("Исходный массив:");
+PrintMatrix(matrix);
+
+int[] temp = new int[columns];
+for (int j = 0; j < columns; j++)
+{
+    temp[j] = matrix[0, j];
+    matrix[0, j] = matrix[rows - 1, j];
+    matrix[rows - 1, j] = temp[j];
+}
+
+Console.WriteLine("Измененный массив:");
+PrintMatrix(matrix);
+Console.ReadLine();
+
+static void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
